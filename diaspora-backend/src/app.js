2b97ad55e7a183575641d100efa8885cc30f7ce6
boussocisse <boussoc89@gmail.com>
2026-05-14 01:36:52 +0200

    preparation pour deploiement Railway

--- diaspora-backend/src/app.js
+++ diaspora-backend/src/app.js
@@ -15,10 +15,12 @@ const app = express();
 // Middlewares de sécurité
 app.use(helmet());
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL || "http://localhost:5173",
+      credentials: true,
+    }),
+  ),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
